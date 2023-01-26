e9249c3a3ffde5956fed1c2a0bb6d830108f8338
Alex <alex.mayka20@yandex.ru>
2023-01-27 01:13:22 +0300

    Process 4

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# selenium_python
-Обучение на selenium 
+# Selenium_python
+<h3>Обучение на selenium</h3> 
+
+Скрипт для автоматизации получения данных из сайта "Госуслуги"
+
+Цель:
+
+Получить информицию о статусе акредитации it-компаний из сайта "Госуслуги" (https://www.gosuslugi.ru/itorgs) 
+
+Входящие данные: <br>
+Файл '.csv' с ИНН организаций (..\inn_org\inn_org.csv)
+
+![input.png](img\input.png)
+
+Технологии:<br>
+<ul>
+    <li>Язык программирования: Python 3.11.0</li>
+    <li>Библиотеки:
+        <ul>
+            <li>Pandas</li>
+            <li>Selenium</li>
+            <li>Numpy</li>
+            <li>Logging</li>
+        </ul>
+    </li>
+</ul>
+
+![example_performance.gif](img%2Fexample_performance.gif)
+
+Процесс работы:
+
+<ol>
+    <li>Извлечение инн из ".csv" файла;</li>
+    <li>Каждый из номеров проверить на сайте (selenium);</li>
+    <li>Результат проверки номера записать в Data Frame (pandas);</li>
+    <li>Записать результат в ".csv" файл.</li>
+</ol>
+
+Выходные данные:
+
+Файл log_for_work.log. Лог-файл работы скрипта (отслеживание статуса выполнения)
+
+![log.png](img%2Flog.png)
+
+Файл с результатом работы Result_work_selenium.csv состоящий из:
+<ul>
+    <li>Колонка 1: inn - Идентификационный номер налогоплательщика компании</li>
+    <li>Колонка 2: name_of_company - Наименование компании</li>
+    <li>Колонка 3: IT_accreditation - Статус аккредитации-</li>
+</ul>
+
+![out.png](img\out.png)
--- main.py
+++ main.py
@@ -2,42 +2,44 @@
 # Importing libraries
 
 import logging
-
 import time
+import pandas as pd
+import numpy as np
 
 from selenium import webdriver
-from selenium.common import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
 def logging_work():
     """
-    Настройки для логгирования скрипта
-    Script logging settings
+    Настройки для логгирования скрипта;
+    Script logging settings;
     """
 
     py_logger = logging.getLogger(__name__)
     py_logger.setLevel(logging.INFO)
 
-    py_handler = logging.FileHandler(f"{__name__}.log", mode='w')
+    py_handler = logging.FileHandler(f"log_for_work.log", mode='w')
     py_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
 
     py_handler.setFormatter(py_formatter)
     py_logger.addHandler(py_handler)
 
-    py_logger.info(f"Начало логгироваия {__name__}")
+    py_logger.info(f"Begin logging {__name__}")
 
     return py_logger
 
 
-def read_json_inn_com(logger):
+def read_csv_inn_com(logger):
     """
-    Функция для получения инн компаний из csv-файла
-    Function to get company TIN from csv file
-    :return: inn_list - список инн компаний (list of inn companies)
+    Функция для получения инн компаний из csv-файла;
+    Function to get company TIN from csv file;
+
+    :return: inn_list - список инн компаний (list of inn companies);
     """
 
     logger.info(f'Start reading file from inn')
@@ -55,7 +57,72 @@ def read_json_inn_com(logger):
         return False
 
 
-def work_selenium(logger, inn_list):
+def check_response(driver, df, inn):
+    """
+    Функция проверки и записи в df;
+    The function of checking and writing to df;
+
+    :input:
+            driver - элемент класса браузера (browser class element);
+            df - Data Frame для записи результатов (Data Frame for recording results);
+            inn - инн компании (inn company)
+
+    :return:
+            df - Data Frame с новыми данными ( Data Frame with new data);
+    """
+
+    delay = 2
+    logger.info(f'Start_check_inn - {inn}')
+    try:
+        check_element_response = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.XPATH, "//img[@class='img-ok']"))
+        )
+        elem_text_response = driver.find_element(By.XPATH, "//div[@class='mt-12 text-help']")
+
+        name_of_company = " ".join(elem_text_response.text.split(',')[:-1])
+
+        logger.info(f'True_check_inn - {inn}')
+        df = df.append(
+            {
+                'inn': inn,
+                'name_of_company': name_of_company,
+                'IT_accreditation': "True"
+            },
+            ignore_index=True
+        )
+        logger.info(f'Finish_check_inn - {inn}')
+        return df
+
+    except NoSuchElementException:
+        logger.info(f'False_check_inn - {inn}')
+        df = df.append(
+            {
+                'inn': inn,
+                'name_of_company': np.NAN,
+                'IT_accreditation': "False"
+            },
+            ignore_index=True
+        )
+        logger.info(f'Finish_check_inn - {inn}')
+        return df
+
+
+def work_selenium(logger, inn_list, index_inn_list=0, df_verified_inn=pd.DataFrame()):
+    """
+    Функция работы selenium;
+    Selenium work function;
+
+    :input:
+            logger - настройки логгирования (logging settings);
+            inn_list - список с инн компаний (list with inn of companies);
+            index_inn_list - индекс списка inn_list, с которого начнется новая проверка
+            (the index of the inn_list from which the new check will start)
+            df_verified_inn - Data Frame для записи результатов (Data Frame for recording results);
+
+    :return:
+            df_verified_inn - Data Frame с результатами проверки (Data Frame with test results);
+    """
+
     try:
         logger.info(f'Driver setup and site connection')
 
@@ -64,34 +131,52 @@ def work_selenium(logger, inn_list):
 
         logger.info(f'Connection succeeded')
 
-    except Exception as Error_connect:
-        logger.exception(f'Error connecting to site. {Error_connect}')
+    except Exception as Error_connect_selenium:
+        logger.exception(f'Error connecting to site. {Error_connect_selenium}')
         return False
 
     try:
-        logger.info(f'Getting started with the site')
+        logger.info(f'Getting started with the site {index_inn_list}')
         driver.get('https://www.gosuslugi.ru/itorgs')
         delay = 6  # seconds
-        try:
-            for i in inn_list:
-                elem_search = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//input[@class='search-input ng-untouched ng-pristine ng-valid']")))
-                elem_search.send_keys(i)
-                elem_search.send_keys(Keys.RETURN)
-                elem_search = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//a[@class='link-plain small']")))
-                elem_search.click()
+        while index_inn_list < len(inn_list):
+            try:
+                inn = inn_list[index_inn_list]
+                logger.info(f'Search_inn_{inn}')
+                elem_search_string = WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+                    (By.XPATH, "//input[@class='search-input ng-untouched ng-pristine ng-valid']")))
+                elem_search_string.clear()
+                elem_search_string.send_keys(inn)
+                time.sleep(0.3)
+                elem_search_string.send_keys(Keys.ENTER)
 
-            time.sleep(10)
-        except TimeoutException:
-            print("Loading took too much time!")
+                df_verified_inn = check_response(driver, df_verified_inn, inn)
 
-        logger.info(f'End of the site')
+                logger.info(f'Back_in_search_string - {inn}')
+                elem_return_button = WebDriverWait(driver, delay).until(
+                    EC.presence_of_element_located((By.XPATH, "//a[@class='link-plain small']")))
+                elem_return_button.click()
 
-    except Exception as Error_work_site:
-        logger.exception(f'Error while working with the site. {Error_work_site}')
-        return False
+                index_inn_list += 1
+                if index_inn_list % 25 == 0:
+                    driver.close()
+                    logger.info(f'new_crusade')
+                    time.sleep(60)
+                    return work_selenium(logger, inn_list, index_inn_list, df_verified_inn)
+
+            except Exception as Error_work_selen:
+                logger.exception(f'Error search inn {inn}. {Error_work_selen}')
+
+        logger.info(f'Finish work selenium')
+        return df_verified_inn
+
+    except Exception as Error_connect_site:
+        logger.exception(f'Error connecting to site. {Error_connect_site}')
+        return df_verified_inn
 
 
 if __name__ == '__main__':
     logger = logging_work()
-    inn_list = read_json_inn_com(logger)
-    work_selenium(logger, inn_list)
+    inn_list = read_csv_inn_com(logger)
+    df_verified_inn = work_selenium(logger, inn_list)
+    df_verified_inn.to_csv(f'Result_work_selenium.csv', index=False, encoding='utf-8-sig')
